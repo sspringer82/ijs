30587068d4a26c348fc2175b0b0b612bf5639f5f
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2024-11-15 15:56:54 +0100

    book create form

--- app/actions/book.ts
+++ app/actions/book.ts
@@ -1,6 +1,9 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
+import { errorExtractor } from '../util/errorExtractor';
+import { redirect } from 'next/navigation';
+import { release } from 'os';
 
 export async function deleteBook(id: string): Promise<void> {
   const response = await fetch(`http://localhost:3001/books/${id}`, {
@@ -11,4 +14,39 @@ export async function deleteBook(id: string): Promise<void> {
   }
 
   revalidatePath('/books');
+}
+
+export async function createBook(
+  errorState: string,
+  formData: FormData
+): Promise<string> {
+  try {
+    const book = {
+      isbn: formData.get('isbn') as string,
+      title: formData.get('title') as string,
+      author: formData.get('author') as string,
+      pages: formData.get('pages') as string,
+      language: formData.get('language') as string,
+      rating: formData.get('rating') as string,
+      release: new Date().toISOString(),
+    };
+
+    // const book = Object.fromEntries(formData.entries());
+
+    const response = await fetch('http://localhost:3001/books', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(book),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to create book');
+    }
+  } catch (error) {
+    return errorExtractor(error);
+  }
+  revalidatePath('/books');
+  redirect('/books');
 }
